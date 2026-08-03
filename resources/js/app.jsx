750a52d9aef1a0de0cd7e397bd8dd1a35cea2d94
neoooxp <tlim4@paragoniu.edu.kh>
2026-08-04 00:16:26 +0700

    Integrate Sentry for Laravel and React error monitoring

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,9 +2,16 @@ import './bootstrap';
 import '../css/app.css';
 import 'leaflet/dist/leaflet.css';
 
+import * as Sentry from '@sentry/react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    environment: import.meta.env.MODE,
+    enabled: import.meta.env.PROD,
+});
 
 import { SessionProvider } from './context/SessionContext';
 import AppShell from './layouts/AppShell';
